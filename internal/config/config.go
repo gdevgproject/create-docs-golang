7d931eea2e48f6832d0042fd81da1b72308a9e80
Vũ Đức Minh <gdev.gproject@gmail.com>
2026-08-02 09:30:02 +0700

    chore: prepare v1.0.3 release with embedded icon and performance improvements

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,8 +9,8 @@ import (
 )
 
 var (
-	// Version can be set at build time via -ldflags "-X codedocs/internal/config.Version=v1.0.0"
-	Version    = "v1.0.0"
+	// Version can be set at build time via -ldflags "-X codedocs/internal/config.Version=v1.0.3"
+	Version    = "v1.0.3"
 	GitHubRepo = "gdevgproject/create-docs-golang"
 )
 
